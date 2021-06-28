83b358a047a583fe48e389c823d16db0880133cb
Clonkk <sidger@zoho.com>
2021-06-28 10:30:27 +0200

    add iterators tests; use camelCase in tests

--- tests/tstring.nim
+++ tests/tstring.nim
@@ -47,7 +47,7 @@ suite "CppString":
     var s: CppString = initCppString "Hello Nim!"
 
     check s.size == s.length
-    check s.max_size > s.size
+    check s.maxSize > s.size
 
     s.resize 5
 
@@ -66,7 +66,7 @@ suite "CppString":
 
     s = initCppString("Hello")
     s.reserve(s.capacity*2)
-    s.shrink_to_fit
+    s.shrinkToFit
 
     # check s.length == s.capacity # implementation dependent.
       # Does not allways hold
@@ -124,7 +124,7 @@ suite "CppString":
     s2 = initCppString "I say Bye!"
     s.append(s2.cbegin+5, s2.cend)
     check s == "Hello Nim! Welcome!!!!! :) :). Bye!".cstring
-    s.push_back '!'
+    s.pushBack '!'
     check s == "Hello Nim! Welcome!!!!! :) :). Bye!!".cstring
 
     s = initCppString ""
@@ -243,13 +243,13 @@ suite "CppString":
     s2 = initCppString "hello"
     s.swap s2
     check s == "hello".cstring
-    s.pop_back
+    s.popBack
     check s == "hell".cstring
 
   test "test string operations":
     var s: CppString = initCppString "Hello Nim!"
 
-    check s.c_str == "Hello Nim!".cstring
+    check s.cStr == "Hello Nim!".cstring
     check s.data[] == 'H'
 
     var s2: CppString = initCppString "Hello"
@@ -281,70 +281,70 @@ suite "CppString":
 
     s = initCppString "Please, replace the vowels in this sentence by asterisks."
     s2 = initCppString "aeiou"
-    var found = s.find_first_of(s2);
+    var found = s.findFirstOf(s2);
     while found != std_npos:
       s[found] = '*'
-      found = s.find_first_of(s2, found+1)
+      found = s.findFirstOf(s2, found+1)
 
     check s == "Pl**s*, r*pl*c* th* v*w*ls *n th*s s*nt*nc* by *st*r*sks.".cstring
 
     s = initCppString "Please, replace the vowels in this sentence by asterisks."
-    found = s.find_first_of("aeiou");
+    found = s.findFirstOf("aeiou");
     while found != std_npos:
       s[found] = '*'
-      found = s.find_first_of("aeiou", found+1)
+      found = s.findFirstOf("aeiou", found+1)
 
     check s == "Pl**s*, r*pl*c* th* v*w*ls *n th*s s*nt*nc* by *st*r*sks.".cstring
 
     s = initCppString "Please, replace the vowels in this sentence by asterisks."
-    found = s.find_first_of("aeiou", 0, 3);
+    found = s.findFirstOf("aeiou", 0, 3);
     while found != std_npos:
       s[found] = '*'
-      found = s.find_first_of("aeiou", found+1, 3)
+      found = s.findFirstOf("aeiou", found+1, 3)
 
     check s == "Pl**s*, r*pl*c* th* vow*ls *n th*s s*nt*nc* by *st*r*sks.".cstring
-    check s.find_first_of('l', 3) == 11
+    check s.findFirstOf('l', 3) == 11
 
     s = initCppString "/usr/bin/man"
     s2 = initCppString "/\\"
 
-    found = s.find_last_of(s2)
+    found = s.findLastOf(s2)
     check s.substr(found+1) == "man".cstring
-    found = s.find_last_of(s2, found-1)
+    found = s.findLastOf(s2, found-1)
     check s.substr(found+1) == "bin/man".cstring
-    found = s.find_last_of("/\\")
+    found = s.findLastOf("/\\")
     check s.substr(found+1) == "man".cstring
-    found = s.find_last_of("/\\", found-1)
+    found = s.findLastOf("/\\", found-1)
     check s.substr(found+1) == "bin/man".cstring
-    found = s.find_last_of("/\\lll", std_npos, 3)
+    found = s.findLastOf("/\\lll", std_npos, 3)
     check s.substr(found+1) == "man".cstring
-    found = s.find_last_of("/\\", found-1, 3)
+    found = s.findLastOf("/\\", found-1, 3)
     check s.substr(found+1) == "bin/man".cstring
-    found = s.find_last_of('m')
+    found = s.findLastOf('m')
     check s.substr(found) == "man".cstring
-    found = s.find_last_of('b', found-1)
+    found = s.findLastOf('b', found-1)
     check s.substr(found) == "bin/man".cstring
 
     s = initCppString "1293a456b7"
     s2 = initCppString "123456789"
 
-    check s.find_first_not_of(s2) == 4
-    check s.find_first_not_of(s2, 5) == 8
-    check s.find_first_not_of("123456789".cstring) == 4
-    check s.find_first_not_of("123456789".cstring, 5) == 8
-    check s.find_first_not_of("123456789".cstring, 0, 3) == 2
-    check s.find_first_not_of("123456789".cstring, 4, 3) == 4
-    check s.find_first_not_of('1') == 1
-    check s.find_first_not_of('1', 1) == 1
+    check s.findFirstNotOf(s2) == 4
+    check s.findFirstNotOf(s2, 5) == 8
+    check s.findFirstNotOf("123456789".cstring) == 4
+    check s.findFirstNotOf("123456789".cstring, 5) == 8
+    check s.findFirstNotOf("123456789".cstring, 0, 3) == 2
+    check s.findFirstNotOf("123456789".cstring, 4, 3) == 4
+    check s.findFirstNotOf('1') == 1
+    check s.findFirstNotOf('1', 1) == 1
 
-    check s.find_last_not_of(s2) == 8
-    check s.find_last_not_of(s2, 5) == 4
-    check s.find_last_not_of("123456789".cstring) == 8
-    check s.find_last_not_of("123456789".cstring, 5) == 4
-    check s.find_last_not_of("123456789".cstring, std_npos, 3) == 9
-    check s.find_last_not_of("123456789".cstring, 4, 3) == 4
-    check s.find_last_not_of('1') == 9
-    check s.find_last_not_of('1', 1) == 1
+    check s.findLastNotOf(s2) == 8
+    check s.findLastNotOf(s2, 5) == 4
+    check s.findLastNotOf("123456789".cstring) == 8
+    check s.findLastNotOf("123456789".cstring, 5) == 4
+    check s.findLastNotOf("123456789".cstring, std_npos, 3) == 9
+    check s.findLastNotOf("123456789".cstring, 4, 3) == 4
+    check s.findLastNotOf('1') == 9
+    check s.findLastNotOf('1', 1) == 1
 
     s = initCppString "green apple"
     s2 = initCppString "red apple"
@@ -387,8 +387,8 @@ suite "CppString":
     check not (s1 >= s2)
     check s1 >= "aaaaaaaaaaaaa".cstring
 
-  test "iterators native":
-    var s1 = toCppString("123456789")
+  test "Nim iterators":
+    let s1 = toCppString("123456789")
     block:
       var i = 0
       for c in s1:
@@ -400,4 +400,17 @@ suite "CppString":
         check idx == i.csize_t
         inc(i)
         check cchar(i.uint8+uint8('0')) == c
+
+  test "mutable Nim iterators":
+    var s1 = toCppString("123456789")
+    for c in s1.mitems:
+      c = 'a'
+    check s1.toString == "aaaaaaaaa"
+
+    var i = 0
+    for idx, c in s1.mpairs:
+      check idx == i.csize_t
+      inc(i)
+      c = 'b'
+    check s1.toString == "bbbbbbbbb"
 
