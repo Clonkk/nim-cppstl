dd9e6b4790447d4f219abdb89f8989d0c2a4d157
Clonkk <rf.clonk@linuxmail.org>
2024-08-19 10:39:05 +0200

    add --cc:gcc to macosx platform

--- tests/destroy_bug_15.nim
+++ tests/destroy_bug_15.nim
@@ -3,24 +3,23 @@ import cppstl/std_vector
 type Foo = object
   x: int
 
-proc `=destroy`*(a: var Foo) {.inline.} =
+proc `=destroy`*(a: Foo) =
   echo ("=destroy", a.x)
 
 proc main =
   var v = initCppVector[Foo]()
 
+  echo v
   v.add Foo(x: 10)
   v.add Foo(x: 11)
-
-  echo "ok0"
   echo v
+
   v.add Foo(x: 12)
+  v.add Foo(x: 13)
 
-  echo "ok1"
+  echo "-----------"
   echo v
-
   # Clear should call object destructor
   v.clear()
-  echo "ok2"
 
 main()
